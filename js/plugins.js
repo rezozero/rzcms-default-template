d539bbe82ed88c1562c2fcd29449b032198e41a7
Maxime Bérard <maxime@rezo-zero.com>
2014-10-16 17:33:38 +0200

    Added generate random number in plugins js & re-organized, documented it better

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,7 @@
+/**
+ * Check if device is mobile or not.
+ * @type {Object}
+ */
 var isMobile = {
     Android: function() {
         return navigator.userAgent.match(/Android/i);
@@ -19,6 +23,12 @@ var isMobile = {
     }
 };
 
+
+/**
+ * Check if an element is set.
+ * @param  {[type]} element [description]
+ * @return {[type]}         [description]
+ */
 var isset = function(element) {
     if (typeof(element) != "undefined") {
         return true;
@@ -26,14 +36,26 @@ var isset = function(element) {
     return false;
 };
 
-// Add class custom
+
+/**
+ * Add class custom.
+ * @param  {[object]} el                [dom element]
+ * @param  {[string]} classToAdd        [class to add]
+ * @return {[type]}                     [description]
+ */
 var addClass = function(el, classToAdd){
 
     if (el.classList) el.classList.add(classToAdd);
     else el.className += ' ' + classToAdd;
 };
 
-// Remove class custom
+
+/**
+ * Remove class custom.
+ * @param  {[object]} el                [dom element]
+ * @param  {[string]} classToRemove     [class to remove]
+ * @return {[type]}                     [description]
+ */
 var removeClass = function(el, classToRemove){
 
     if(el.classList) el.classList.remove(classToRemove);
@@ -45,7 +67,34 @@ var removeClass = function(el, classToRemove){
     }    
 };
 
-// Avoid `console` errors in browsers that lack a console.
+
+/**
+ * Get random number.
+ * @param  {[number]} min [min value]
+ * @param  {[number]} max [max value]
+ * @return {[type]}     [description]
+ */
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+/**
+ * Get random integer.
+ * @param  {[int]} min [min value]
+ * @param  {[int]} max [max value]
+ * @return {[type]}     [description]
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+/**
+ * Avoid `console` errors in browsers that lack a console.
+ * @return {[type]} [description]
+ */
 (function() {
     var method;
     var noop = function () {};
@@ -69,7 +118,10 @@ var removeClass = function(el, classToRemove){
 }());
 
 
-// Replace placeholder
+/**
+ * Replace placeholder for non-HTML5 compatible browsers.
+ * @return {[type]} [description]
+ */
 var replacePlaceholder = function() {
 
     if(!Modernizr.input.placeholder){
@@ -101,7 +153,9 @@ var replacePlaceholder = function() {
 };
 
 
-// jQuery/helper plugins
+/**
+ * JS / jQuery helper & plugins
+ */
 
-// jQuery Spamless
+// --- jQuery Spamless --- //
 (function($){$.fn.dcSpamless=function(options){var defaults={reverse:true,splitDomain:'[dot]',splitName:'[at]',mailto:true};var options=$.extend(defaults,options);return this.each(function(options){var domain=defaults.splitDomain,name=defaults.splitName;var email=$(this).is('a')?$(this).attr('href').replace('mailto:','').replace(domain,'.').replace(name,'@'):$(this).text().replace(domain,'.').replace(name,'@');email=defaults.reverse == true?email.split('').reverse().join(''):email;if($(this).is('a')){$(this).attr('href','mailto:'+email)}else{if(defaults.mailto===true){email='<a href="mailto:'+email+'">'+email+'</a>'}$(this).html(email)}})}})(jQuery);
